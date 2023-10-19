1a5f0cf1675ae5882b9eb18ad074d0a461eadbae
ennaangel <chrisvos1992@gmail.com>
2023-10-19 17:40:24 +0200

    Added test for add property and moce metadata

--- obsidian_markdown/test/tests_movemetadata_to_properties.py
+++ obsidian_markdown/test/tests_movemetadata_to_properties.py
@@ -10,7 +10,7 @@ tags: src/youtubevideo,ToDo/Cook,
 date:  2021-12-05
 People: [[The Dissenter]],[[Louise Barrett]]
 ---"""
-TEXT: str = """
+TEXT: str = """---
 aliases:
 tags: src/youtubevideo,ToDo/Cook,
 date:  2021-12-05
@@ -43,6 +43,8 @@ People: [[The Dissenter]],[[Louise Barrett]]
 def main():
     test_add_property_to_properties()
     test_get_property_value()
+    test_move_metadata_to_properties()
+    test_add_property()
 
 def test_add_property_to_properties():
     print('[INFO] Testing add proprty to propreties')
@@ -77,14 +79,8 @@ People: Henk,[[Louise Barrett]],[[The Dissenter]]
         new_properties = move_metadata_to_properties.add_property_to_properties(properties = parameters['input']['properties'], 
                                                            key = parameters['input']['key'],
                                                            value = parameters['input']['value'])
-        if new_properties != parameters['output']:
-            print(f'[ERROR]{parameters["error_message"]}\n')
-            print('\n-------------------')
-            print(new_properties)
-            print('-------------------\n')
-            errors += 1
-        else:
-            print(f'[INFO] {test} passed')
+        test_check(result = new_properties, base_value = parameters['output'], test = test)
+        
     print(f"[INFO] Failed tests: {errors}\n")
     return
 
@@ -93,6 +89,83 @@ def test_get_property_value():
     assert move_metadata_to_properties.get_property_value(properties = PROPERTIES, key = 'People').strip() == '[[The Dissenter]],[[Louise Barrett]]'
     print('[INFO] get property value passed')
     return
+
+def test_move_metadata_to_properties():
+    new_text = move_metadata_to_properties.move_metadata_to_properties(text = TEXT, 
+                                           key = 'People', 
+                                           metadata_keys = ['Creator', 'People'], 
+                                           removed_metadata_keys = ['People'])
+    test_result = """---
+aliases:
+tags: src/youtubevideo,ToDo/Cook,
+date:  2021-12-05
+People: [[Louise Barrett]],[[The Dissenter]]
+---
+# Louise Barrett, Baboon Societies, Ecology, Embodied Cognition, and Evolutionary Psychology
+---
+## Key notes
+- The brain is the module, not parts of the brain themselves.
+- Cognition arises through a dynamic interaction between an organism and it's environment.
+
+## Random Mentions
+#### Books:
+- Joseph Henrich: Weird culture
+- Matthew cobbs - the idea of the brain
+- [[Addiction by design]]
+---
+### Meta info
+- Creator: [[The Dissenter]], [[Louise Barrett]]
+- Episode: 551
+- Themes: [[Evolution]]
+- Series:
+- Field:
+- Found through:
+- Url: https://www.youtube.com/watch?v=W4vJVrYZBlA
+- Date: 2021-12-05
+""" 
+    test_check(result = new_text, base_value = test_result, test = 'move_metadata_to_properties')
+
+def test_add_property():
+    new_text = move_metadata_to_properties.add_property(text = TEXT, key = 'People', value = 'Henk, Hanny')
+    check_result = """---
+aliases:
+tags: src/youtubevideo,ToDo/Cook,
+date:  2021-12-05
+People: Hanny,Henk,[[Louise Barrett]],[[The Dissenter]]
+---
+# Louise Barrett, Baboon Societies, Ecology, Embodied Cognition, and Evolutionary Psychology
+---
+## Key notes
+- The brain is the module, not parts of the brain themselves.
+- Cognition arises through a dynamic interaction between an organism and it's environment.
+
+## Random Mentions
+#### Books:
+- Joseph Henrich: Weird culture
+- Matthew cobbs - the idea of the brain
+- [[Addiction by design]]
+---
+### Meta info
+- Creator: [[The Dissenter]], [[Louise Barrett]]
+- Episode: 551
+- Themes: [[Evolution]]
+- Series:
+- Field:
+- Found through:
+- Url: https://www.youtube.com/watch?v=W4vJVrYZBlA
+- Date: 2021-12-05
+"""
+    test_check(result = new_text, base_value = check_result, test = 'Add property')
+
+def test_check(result, base_value, test):
+    if result != base_value:
+        print(f"[ERROR] {test} test failed")
+        print("###################")
+        print(result)
+        print("###################")
+    else:
+        print(f'[INFO] {test} succeeded')
+
 
 if __name__ == '__main__':
     main()
