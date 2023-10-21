1c10d1e72f9abf9d61c937e7d709ba89e37a2ab5
ennaangel <chrisvos1992@gmail.com>
2023-10-21 15:46:05 +0200

    Only read markdown files

--- obsidian_markdown/runner.py
+++ obsidian_markdown/runner.py
@@ -8,13 +8,15 @@ folder_path: str = 'D:\\Documents\\Drive\\Obsidian\\Knowledge vault\\Videos epis
 def get_file_list(path: str)-> list:
     all_items = os.listdir(path)
     only_files = [file for file in all_items if isfile(join(path, file))]
-    return only_files
+    only_md_files = [file for file in only_files if file.endswith(".md")]
+    return only_md_files
 
 
 def main():
     """"""
     files: list = get_file_list(folder_path)
     for file in files:
+        print(file)
         full_path = join(folder_path, file)
         move_metadata_to_properties.edit_file(full_path)
 
